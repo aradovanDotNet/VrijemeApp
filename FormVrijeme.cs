97e922e4b1768d592f34832b2982851d08258a05
aradovanDotNet <aradovan@vub.hr>
2026-04-06 10:12:12 +0200

    Predložak za šestu vježbu

---
+++ FormVrijeme.cs
@@ -0,0 +1,11 @@
+namespace VrijemeApp {
+  public partial class FormVrijeme : Form {
+    public FormVrijeme() {
+      InitializeComponent();
+    }
+
+    private void cbGrad_SelectedIndexChanged(object sender, EventArgs e) {
+      // PrikaziPodatkeZaGrad(cbGrad.Text);
+    }
+  }
+}
---
+++ Vrijeme.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace VrijemeApp {
+  public class Vrijeme {
+    public Vrijeme() {
+      // TODO: rukovanje s iznimkama
+      podaci = DohvatiPodatke();
+    }
+
+    public List<VrijemePodatak> DohvatiPodatke() {
+      XmlDocument vrijeme = new XmlDocument();
+      vrijeme.Load("http://vrijeme.hr/hrvatska_n.xml");
+
+      List<VrijemePodatak> podaci = new List<VrijemePodatak>();
+
+      XmlNodeList gradovi = vrijeme.GetElementsByTagName("Grad");
+      foreach (XmlNode grad in gradovi) {
+        podaci.Add(new VrijemePodatak(
+            grad["GradIme"].InnerText,
+            grad["Podatci"]["Temp"].InnerText,
+            grad["Podatci"]["Vlaga"].InnerText,
+            grad["Podatci"]["Tlak"].InnerText
+        ));
+      }
+      return podaci;
+    }
+
+    public IEnumerable<string> DohvatiGradove() {
+      // TODO:
+      return
+        null;
+    }
+
+    public VrijemePodatak DohvatiPodatkeZaGrad(string grad) {
+      // TODO:
+      return
+        null;
+    }
+
+    public IEnumerable<VrijemePodatak> DohvatiNajtoplijeGradove(int kolicina) {
+      // TODO:
+      return
+        null;
+    }
+
+    public IEnumerable<VrijemePodatak> DohvatiNajhladnijeGradove(int kolicina) {
+      // TODO:
+      return
+        null;
+    }
+
+    private List<VrijemePodatak>? podaci;
+  }
+}
